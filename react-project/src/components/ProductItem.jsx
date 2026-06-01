27266b0e2ec60b48ca344cb5fd81a05f87384c31
Yash Arjitwar <yashsmac@Yashs-MacBook-Air.local>
2026-06-01 16:12:42 +0530

    Implement addItem dispatch in ProductItem (add to cart)

--- react-project/src/components/ProductItem.jsx
+++ react-project/src/components/ProductItem.jsx
@@ -1,12 +1,26 @@
 
 
+import { useDispatch } from "react-redux";
+import { addItem } from "../redux/cartSlice";
+
 function ProductItem({ product }) {
+  const dispatch = useDispatch();
+
+  const handleAddToCart = () => {
+    dispatch(addItem({
+      id: product.id,
+      title: product.title,
+      price: product.price,
+      thumbnail: product.thumbnail,
+    }));
+  };
+
   return (
     <div style={{ border: "1px solid #ccc", borderRadius: "8px", padding: "1rem", width: "200px" }}>
       <img src={product.thumbnail} alt={product.title} style={{ width: "100%", height: "auto" }} />
       <h3>{product.title}</h3>
       <p>${product.price}</p>
-      <button>Add to Cart</button>
+      <button onClick={handleAddToCart}>Add to Cart</button>
     </div>
   );
 }
