baed11fec195f4a6e7114fc5a952ca283cc3bb50
Yash Arjitwar <yashsmac@Yashs-MacBook-Air.local>
2026-06-01 14:30:49 +0530

    Build styled NotFound component with link to home

--- react-project/src/components/NotFound.jsx
+++ react-project/src/components/NotFound.jsx
@@ -1,6 +1,15 @@
 
 
+import { Link } from "react-router-dom";
+
 function NotFound() {
-  return <div>404 – Page Not Found</div>;
+  return (
+    <div style={{ textAlign: "center", marginTop: "3rem" }}>
+      <h1>404 – Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <Link to="/">Go back to Home</Link>
+    </div>
+  );
 }
+
 export default NotFound;
