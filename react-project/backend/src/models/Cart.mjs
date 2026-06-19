c9141c42a6d6586d366ef79202b2eaa95144ff24
Yash Arjitwar <yashsmac@Yashs-MacBook-Air.local>
2026-06-19 11:37:30 +0530

    Created Cart model with userId and items array

---
+++ react-project/backend/src/models/Cart.mjs
@@ -0,0 +1,15 @@
+
+
+import mongoose from 'mongoose';
+
+const cartItemSchema = new mongoose.Schema({
+    productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+    quantity: { type: Number, required: true, default: 1 }
+});
+
+const cartSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    items: [cartItemSchema]
+});
+
+export const Cart = mongoose.model('Cart', cartSchema);
